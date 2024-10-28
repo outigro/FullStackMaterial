7bbe370f44c6db83e9efef90ef3e414bd5541fa4
Outi Grotenfelt <94464172+outigro@users.noreply.github.com>
2024-10-28 12:13:03 +0200

    atlas_insert.js

--- WS5 MongoDB/ex0/atlas_insert.js
+++ WS5 MongoDB/ex0/atlas_insert.js
@@ -1,56 +1,54 @@
-// Tuodaan moduuli ohjelmaan
+// Bring the module to the program
 const MongoClient = require("mongodb").MongoClient;
 
-// Määritellään salasana ja yhteysosoite tietokantaan (tämän saa MongoDB Atlas-palvelusta)
+// pass & connection address
 const passwd = "demopass";
 const uri =
   "mongodb+srv://dbuser:" +
   passwd +
   "@cluster0-6tein.mongodb.net/test?retryWrites=true&w=majority";
 
-// Luodaan uusi yhteysolio käyttäen edellä määriteltyä URI:a sekä
-// tarvittavia parametreja
+// Create a new connection object with URI and needed parameters
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-// Määritellään tietokantaan tehtävä kyselu JSON-oliona. Tässä voi käyttää
-// apuna esim. MondoDB Compass -työkalua
+// Introduce query to the database as JSON-object. (MondoDB Compass)
 var query = {
-  title: new RegExp("Mikan uusi")
+  title: new RegExp("Students new")
 };
 
-// Uuden lisättävän tieoalkion kuvaus
+// Introduction of the new data
 var newMovie = {
-  title: "Mikan uusi elokuva",
-  year: "2020",
+  title: "Students new movie",
+  year: "2024",
   imdbID: "12345678",
   type: "movie",
   poster:
     "https://m.media-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg"
 };
 
-// Luodaan yhteys  tietokantaan nimeltä "sample_mflix" ja sieltä kokoelmaan "movies"
+// Create connection to database "sample_mflix" and collection "movies"
 client.connect(err => {
   const collection = client.db("sample_mflix").collection("movies");
   if (err) throw err;
 
-  // Suoritetaan lisäys collection-olion avulla
+  // Make inserting with collection object
   collection.insertOne(newMovie, function(err, r) {
-    // Tulostetaan konsoliin tieto montako alkiota on lisätty (1)
+    // Print out to console how many objects have been inserted (1)
     console.log(r.insertedCount);
   });
 
-  // Tehdään perään vielä tietokantahaku, jotta nähdään lisäyksen menneen perille
-  // Huomaa, että tietokantahaun ehdoksi on vaihdettu uuden lisätyn leffan nimi
+  // Make still a database query to check, that the inserted is also there
+  // Notice, that database check is made with the new movie name
   collection
-    .find(query) // query muuttuja sisältää kyselyn
-    .limit(5) // rajoitetaan tulosjoukko 5:een
+    .find(query) // query variable contains questionary
+    .limit(5) // limit results to 5
     .toArray(function(err, result) {
-      // Palautetaan tulokset JS-taulukkona
+      //Return results in JS-table
       if (err) throw err;
-      console.log(result); // Tulostetaan taulukko ruudulle
-      client.close(); // Suljetaan yhteys
+      console.log(result); // Print out table on screen
+      client.close(); // Close connection
     });
 });
